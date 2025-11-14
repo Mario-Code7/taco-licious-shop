9866ad48bcf064aa976810759d98222a34d403f4
Mario-Code7 <marioroa71@yahoo.com>
2025-11-13 18:18:32 -0600

    updating ui

--- src/main/java/com/pluralsight/tacos/model/StreetTaco.java
+++ src/main/java/com/pluralsight/tacos/model/StreetTaco.java
@@ -4,10 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StreetTaco extends Taco{
-//    public StreetTaco() {
-//        super(0.0, "3-taco" , "corn", false, new ArrayList<>(List.of("carne asada", "onions", "salsa verde", "lime wedges")));
-//    }
-
 
     public StreetTaco() {
         super("Carne Asada", 0.0, "3-Taco", "Corn", false, new ArrayList<>(List.of("Carne Asada", "onions", "salsa", "lime wedges")));
--- src/main/java/com/pluralsight/tacos/model/SuperBurrito.java
+++ src/main/java/com/pluralsight/tacos/model/SuperBurrito.java
@@ -5,9 +5,6 @@ import java.util.List;
 
 public class SuperBurrito extends Taco{
 
-//    public SuperBurrito(){
-//        super(0.0, "Burrito", "flour", false, new ArrayList<>(List.of("carnitas", "cheddar", "pico de gallo", "lettuce", "tomatoes", "birria dipped")));
-//    }
 public SuperBurrito() {
     super("Carnitas", 0.0, "Burrito", "Flour", false, new ArrayList<>(List.of("Carnitas", "cheddar", "pico de gallo", "lettuce", "tomatoes", "birria dipped")));
 }
